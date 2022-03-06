8b5d977ed1c9d361716129ff2b3d715805e0d6d6
Greg Webb <greg.spyderr@gmail.com>
2022-03-07 07:03:08 +0800

    Update SizeType.kt
    Adjust scale to fix text on Shortcut to suit tablets

--- app/src/main/java/com/delet_dis/elementarylauncher/data/models/SizeType.kt
+++ app/src/main/java/com/delet_dis/elementarylauncher/data/models/SizeType.kt
@@ -5,9 +5,9 @@ enum class SizeType(
     val firstGuidelinePercentage: Float,
     val secondGuidelinePercentage: Float
 ) {
-    SMALL(1.1f, 0.2f, 0.705f),
-    SMALL_MEDIUM(1.2f, 0.17f, 0.710f),
-    MEDIUM(1.3f, 0.14f, 0.715f),
-    MEDIUM_LARGE(1.4f, 0.11f, 0.720f),
-    LARGE(1.5f, 0.08f, 0.715f)
+    SMALL(0.7f, 0.2f, 0.705f),
+    SMALL_MEDIUM(0.8f, 0.17f, 0.710f),
+    MEDIUM(0.9f, 0.14f, 0.715f),
+    MEDIUM_LARGE(1.0f, 0.11f, 0.720f),
+    LARGE(1.1f, 0.08f, 0.715f)
 }
